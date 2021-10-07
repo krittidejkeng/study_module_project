74253afe9718de66d67286d6fb25e1354190f054
Non <patsre990@gmail.com>
2021-10-08 01:10:23 +0700

    10082021

--- print.py
+++ print.py
@@ -1,9 +1,20 @@
 # a = 20
 # print("a is %d" %a)
 
-name = 'exp'
-project='runs\detect'
+# name = 'exp'
+# project='runs\detect'
 
-temp = project / name
+# temp = project / name
 
-print("this is",temp)
+# print("this is",temp)
+# import logging
+
+# FORMAT = '%(asctime)-15s %(clientip)s %(user)-8s %(message)s'
+# logging.basicConfig(format=FORMAT)
+# d = {'clientip': '192.168.0.1', 'user': 'fbloggs'}
+# logger = logging.getLogger('tcpserver')
+# logger.warning('Protocol problem: %s', 'connection reset', extra=d)
+# # print("Format is:",FORMAT)
+# print("Format is:",logging.basicConfig(format=FORMAT))
+
+print("YOLOv5 🚀")
